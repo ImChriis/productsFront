9e959a975f598d0baaf457a320ac4c631605c6b5
Chris <christophermedinabarroso@gmail.com>
2025-07-08 16:57:08 -0400

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './@core/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        loadComponent: () => import('./@core/auth/login/login.component').then(m => m.LoginComponent),
+        title: 'Iniciar sesión',
+    },
+    {
+        path: 'register',
+        loadComponent: () => import('./@core/auth/register/register.component').then(m => m.RegisterComponent),
+        title: 'Registrarse',
+    },
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+
+        ]
+    }
+];
